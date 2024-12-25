dc65a1b82c322edfc9cbc55416798dc61d939d8a
tcfshcrw <48719709+tcfshcrw@users.noreply.github.com>
2024-12-26 04:50:48 +0800

    add espnow task back to S3 board

--- ESP32/include/Main.h
+++ ESP32/include/Main.h
@@ -255,7 +255,7 @@ static const uint32_t SECONDS_PER_MINUTE = 60;
 
   //#define BLUETOOTH_GAMEPAD
   #define USB_JOYSTICK
-  // #define ESPNOW_Enable
+  #define ESPNOW_Enable
   #define ESPNow_S3
   #define SERIAL_COOMUNICATION_TASK_DELAY_IN_MS 5
   //#define ESPNow_Pairing_function
