c3683b3f0374f9677f0a0db0c259f052def27c94
mirel <mirel.t.lazar@gmail.com>
2025-02-03 22:06:20 +0000

    restore default pins for V3 and disable wireless

--- Pedal/include/Main.h
+++ Pedal/include/Main.h
@@ -121,26 +121,26 @@ static const uint32_t SECONDS_PER_MINUTE = 60;
 // V3 version of dev PCB for regular ESP32
 #if PCB_VERSION == 3
   // ADC defines
-  #define PIN_DRDY 19// 19 --> DRDY
-  #define PIN_RST  15 // X --> X
-  #define PIN_SCK 16 // 16 -->SCLK
-  #define PIN_MISO 18 // 18 --> DOUT
-  #define PIN_MOSI 17 // 17 --> DIN
-  #define PIN_CS 21 // 21 --> CS
-
-  // stepper pins
-  #define dirPinStepper    22
-  #define stepPinStepper   23
-  //analog output pin
-  #define D_O 25 
+  #define PIN_DRDY 4  //19 // 19 --> DRDY
+  #define PIN_RST 15  // X --> X
+  #define PIN_SCK 18  //12 //16 // 16 -->SCLK
+  #define PIN_MISO 19 //18 // 18 --> DOUT
+  #define PIN_MOSI 23 // 14 //17 // 17 --> DIN
+  #define PIN_CS 5    //23 // 21 --> CS
   //I2Cpins
   #define I2C_SDA 32
   #define I2C_SCL 33
+  // stepper pins
+  #define dirPinStepper 32  //22
+  #define stepPinStepper 33 //23
+  //analog output pin
+  #define D_O 25
 
   // level shifter is present on this PCB design
   #define SENSORLESS_HOMING true
   #define ISV57_TXPIN 27 //17
   #define ISV57_RXPIN 26 // 16
+
   //pedal assignment
   // #define PEDAL_ASSIGNMENT
   // #define CFG1 15
@@ -152,17 +152,17 @@ static const uint32_t SECONDS_PER_MINUTE = 60;
   // HBRK needs high5v/3v for 100% input
   #define FANATEC_analog_output
   //#define Using_I2C_Sync
-  #define ESPNOW_Enable
-  #define ESPNow_ESP32
+  //#define ESPNOW_Enable
+  //#define ESPNow_ESP32
   //#define I2C_slave_address 0x15
-  #define BLUETOOTH_GAMEPAD
+  //#define BLUETOOTH_GAMEPAD
   //#define USB_JOYSTICK
   #define SERIAL_COOMUNICATION_TASK_DELAY_IN_MS 1
   //#define ESPNow_Pairing_function
   //#define Pairing_GPIO 13
   //#define ESPNow_debug_rudder
-  #define OTA_update_ESP32
-  #define BRAKE_RESISTOR_PIN 13
+  //#define OTA_update_ESP32
+  //#define BRAKE_RESISTOR_PIN 13
   
 #endif
 
